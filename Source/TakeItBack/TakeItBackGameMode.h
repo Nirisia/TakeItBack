08db0db7d756f6637477a0dd0164d56a13758eb4
Baptiste SUZON <b.suzon@student.isartdigital.com>
2020-10-01 15:00:52 +0200

    Init of Unreal Project

---
+++ Source/TakeItBack/TakeItBackGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TakeItBackGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATakeItBackGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATakeItBackGameMode();
+};
+
+
+
