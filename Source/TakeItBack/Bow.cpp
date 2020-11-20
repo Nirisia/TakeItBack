357500ba8ae2270cee70df602d89647eef6354df
Baptiste SUZON <b.suzon@student.isartdigital.com>
2020-11-20 12:42:36 +0100

    Bow can now aim in Z-axis

--- Source/TakeItBack/Bow.cpp
+++ Source/TakeItBack/Bow.cpp
@@ -4,7 +4,11 @@
 #include "Bow.h"
 
 #include "Arrow.h"
+#include "EnemyCharacter.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Engine/Engine.h"
+#include "Engine/EngineTypes.h"
+#include "Kismet/GameplayStatics.h"
 
 ABow::ABow() : Super()
 {
@@ -22,10 +26,28 @@ void ABow::ShootArrow()
     SpawnParam.Owner = GetParentActor();
     ABaseCharacter* BaseCharacter = Cast<ABaseCharacter>(GetParentActor());
     const FVector SpawnLocation = BaseCharacter->GetMesh()->GetSocketLocation("RightWeaponShield");
-    AArrow* ShotArrow = GetWorld()->SpawnActor<AArrow>(Arrow.Get(), SpawnLocation, GetParentActor()->GetActorForwardVector().Rotation(), SpawnParam);
+    AArrow* ShotArrow = GetWorld()->SpawnActor<AArrow>(Arrow.Get(), SpawnLocation,
+                                                       GetParentActor()->GetActorForwardVector().Rotation(),
+                                                       SpawnParam);
     if (ShotArrow)
     {
         ShotArrow->SetDamage(GetCurrentDamage());
+        AEnemyCharacter* EnemyCharacter = Cast<AEnemyCharacter>(BaseCharacter);
+        if (EnemyCharacter)
+        {
+            FVector NewVelocity;
+
+            // Yeah that's bad but it's the easiest way now to get player character from the bow
+            // TODO: Replace this by a proper implementation
+            FVector PlayerLocation = UGameplayStatics::GetPlayerPawn(GetWorld(), 0)->GetActorLocation();
+
+            UGameplayStatics::SuggestProjectileVelocity(GetWorld(), NewVelocity, ShotArrow->GetActorLocation(),
+                                                        PlayerLocation, ShotArrow->GetInitialVelocity(), false, 0, 0, ESuggestProjVelocityTraceOption::DoNotTrace);
+            GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Blue,
+                                             FString::Printf(
+                                                 TEXT("x:%f, y:%f, z:%f"), NewVelocity.X, NewVelocity.Y,
+                                                 NewVelocity.Z));
+            ShotArrow->SetVelocity(NewVelocity);
+        }
     }
 }
-
